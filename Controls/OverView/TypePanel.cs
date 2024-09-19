3009bad5603c2dd8493a3c14b6f641c587dd463e
vancong2305 <truongvancong230502@gmail.com>
2024-09-20 00:06:01 +0700

    init: init project

---
+++ Controls/OverView/TypePanel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace DesignFrame.Controls.OverView
+{
+    public class TypePanel : Panel
+    {
+        private int shadowSize = 30;
+
+        public TypePanel()
+        {
+            this.BackColor = Color.Transparent;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawShadow(e.Graphics);
+        }
+
+        private void DrawShadow(Graphics g)
+        {
+            // Define the color and size of the shadow
+            Color shadowColor = Color.Black;
+            int depth = shadowSize;
+
+            // Draw gradient shadow for all four sides
+
+            // Top shadow
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                new Rectangle(0, 0, this.Width, depth),
+                Color.FromArgb(0, shadowColor),  // Transparent color
+                Color.FromArgb(180, shadowColor), // Opaque color
+                LinearGradientMode.Vertical))
+            {
+                g.FillRectangle(brush, new Rectangle(0, 0, this.Width, depth));
+            }
+
+            // Bottom shadow
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                new Rectangle(0, this.Height - depth, this.Width, depth),
+                Color.FromArgb(180, shadowColor), // Opaque color
+                Color.FromArgb(0, shadowColor),  // Transparent color
+                LinearGradientMode.Vertical))
+            {
+                g.FillRectangle(brush, new Rectangle(0, this.Height - depth, this.Width, depth));
+            }
+
+            // Left shadow
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                new Rectangle(0, 0, depth, this.Height),
+                Color.FromArgb(0, shadowColor),  // Transparent color
+                Color.FromArgb(180, shadowColor), // Opaque color
+                LinearGradientMode.Horizontal))
+            {
+                g.FillRectangle(brush, new Rectangle(0, 0, depth, this.Height));
+            }
+
+            // Right shadow
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                new Rectangle(this.Width - depth, 0, depth, this.Height),
+                Color.FromArgb(180, shadowColor), // Opaque color
+                Color.FromArgb(0, shadowColor),  // Transparent color
+                LinearGradientMode.Horizontal))
+            {
+                g.FillRectangle(brush, new Rectangle(this.Width - depth, 0, depth, this.Height));
+            }
+        }
+    }
+}
---
+++ Controls/StartView/Nickname.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesignFrame.Controls.StartView
+{
+    public partial class Nickname : Form
+    {
+        public Nickname()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None; // Remove border for custom shadow
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.UserPaint |
+                          ControlStyles.AllPaintingInWmPaint |
+                          ControlStyles.OptimizedDoubleBuffer |
+                          ControlStyles.ResizeRedraw, true);
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+    }
+}
